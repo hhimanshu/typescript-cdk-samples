f01af9148e71f7339b3df0a10017cd4150061051
hhimanshu <dev.atwork@yahoo.com>
2022-12-29 08:46:22 -0800

    feat(): create a simple lambda with Function URL

---
+++ openapi/3_openapi_import_lambda_integration/test/3_openapi_import_lambda_integration.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as 3OpenapiImportLambdaIntegration from '../lib/3_openapi_import_lambda_integration-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/3_openapi_import_lambda_integration-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new 3OpenapiImportLambdaIntegration.3OpenapiImportLambdaIntegrationStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
